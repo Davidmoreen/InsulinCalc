8a1dea6113356c5b3f3c28929b93e59a29e5d87d
David <davidmoreen@gmail.com>
2011-10-03 21:51:33 -0400

    Initial Commit

---
+++ Insulin/InsulinAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  InsulinAppDelegate.h
+//  Insulin
+//
+//  Created by David Moreen on 10/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InsulinAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Insulin/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Insulin
+//
+//  Created by David Moreen on 10/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "InsulinAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	int retVal = 0;
+	@autoreleasepool {
+	    retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([InsulinAppDelegate class]));
+	}
+	return retVal;
+}
